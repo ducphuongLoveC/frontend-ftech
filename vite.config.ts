bce2f8f23e2f79b2b77c02c2372b907381c86144
Vinhd768 <vinhd768@gmail.com>
2025-02-06 08:50:13 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react-swc";
 
-import * as path from "path";
+  import * as path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
-      },
-    ],
-  },
-});
+  // https://vite.dev/config/
+  export default defineConfig({
+    plugins: [react()],
+    base: "/",
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+    server: {
+      fs: {
+        strict: false,
+      }
+    },
+    resolve: {
+      alias: [
+        {
+          find: "@",
+          replacement: path.resolve(__dirname, "src"),
+        },
+      ],
+    },
+  });
