cc4f6efb6b6f1c93fbfb0f011b79f6dee470a677
Vinhd768 <vinhd768@gmail.com>
2025-02-07 19:24:04 +0700

    fixxxxx

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: import.meta.env.VITE_URL_SERVER,
   timeout: 240000,
   headers: {
     'Content-Type': 'application/json',
